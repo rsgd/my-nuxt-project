e9c234360e263e3f9063bf1d8b7792fa2f83dae6
Raffael Stüken <r@humans.am>
2021-05-04 22:37:20 +0200

    Remove obsolete code

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,8 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">my-nuxt-project</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="P-2">my-nuxt-project</h1>
+      <div class="links"></div>
     </div>
   </div>
 </template>
@@ -42,16 +25,6 @@ export default {}
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
 }
 
 .subtitle {
