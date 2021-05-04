32d3b6623dbd46f308fbe0e68f9aa1a36b0e5364
Raffael Stüken <r@humans.am>
2021-05-04 23:23:56 +0200

    Update layout

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="NuxtLogo">Hello World</div>
+  <div class="NuxtLogo text-blue-600">Hello World</div>
 </template>
 
 <style>
 .NuxtLogo {
   animation: 1s appear;
   margin: auto;
+  font-size: 2rem;
 }
 
 @keyframes appear {
